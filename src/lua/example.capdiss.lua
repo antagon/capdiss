53920b58a3ca2e5f136c63115a28e708f56a2285
antagon <antagon@codeward.org>
2016-07-09 17:46:13 +0200

    New example script. Fix param order.

--- src/lua/example.capdiss.lua
+++ src/lua/example.capdiss.lua
@@ -7,12 +7,11 @@ local i
 
 function capdiss.begin (filename, link_type)
 	-- Not much to do here, just initialize the counter variable
-	i = 0
-	io.write (string.format ("Begin parsing '%s'...\n", filename))
+	io.write (string.format ("Begin parsing '%s' (linktype: %s)...\n", filename, link_type))
 end
 
-function capdiss.each (ts, frame)
-	i = i + 1
+function capdiss.each (frame, ts, num)
+	i = num
 	io.write (string.format ("%s :: pkt no. %d\n", os.date ("%Y-%m-%d %H:%M:%S", ts), i))
 end
 
